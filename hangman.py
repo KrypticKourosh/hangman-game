86122cc4f7045947ef9c170daa20d7a9d71243be
Behnam Dadashi <kryptickourosh@gmail.com>
2025-02-08 14:43:26 -0800

    feat: play again and main.py clean up

--- hangman.py
+++ hangman.py
@@ -1,5 +1,48 @@
 import requests
 
+def play_hangman():
+   print("--------Welcome to hangman game--------")
+   word_len = get_word_len()
+   random_word = fetch_random_word(word_len)
+   hint = ["_"] * len(random_word)
+   wrong_guess_count = 0
+   guessed_letters = []
+
+   is_running = True
+   while is_running:
+      display_art(wrong_guess_count)
+      display_hint(hint)
+
+      while True:
+         guess = input("Enter letter: ")
+         flag = check_guess(guessed_letters, guess=guess)
+         if flag == "length_error":
+            print("letter should be 1 character long, try again..")
+            continue
+         elif flag == "alphabetic_error":
+            print("letter should be alphabetic, try again..")
+            continue
+         elif flag == "repeated_error":
+            print("you have already guessed that letter, try another one..")
+            continue
+         else:
+            break
+
+      if guess in random_word:
+         hint = update_hint(random_word, hint, guess)
+      else:
+         wrong_guess_count += 1
+      guessed_letters.append(guess)
+
+      flag = check_win_condition(wrong_guess_count, hint)
+      if flag == "won":
+         print(f"You won, total incorrect guesses: {wrong_guess_count}")
+         is_running = False
+      elif flag == "lost":
+         display_art(6) #full hangman
+         print(f"You lost, the word was: {random_word}")
+         is_running = False
+
 def display_art(wrong_guess_count):
    hangman_art = {0:("   ",
                      "   ",
@@ -88,3 +131,15 @@ def get_word_len():
       else:
          return word_len
       
+def ask_play_again():
+   yes_answers = ["YES", "Y", "YA", "YEA", "YEAH", "1"]
+   no_answers = ["NO", "N", "NOT", "NA", "NOPE", "0"]
+   while True:
+      flag = input("Do you want to play again(Y/N): ").upper()
+      if flag in yes_answers:
+         return True
+      elif flag in no_answers:
+         return False
+      else:
+         print("Invalid input, try again..")
+         continue
--- main.py
+++ main.py
@@ -1,47 +1,16 @@
 import hangman
+from time import sleep
 
 def main():
-   print("--------Welcome to hangman game--------")
-   word_len = hangman.get_word_len()
-   random_word = hangman.fetch_random_word(word_len)
-   hint = ["_"] * len(random_word)
-   wrong_guess_count = 0
-   guessed_letters = []
+   play_again = True #initial
+   while play_again:
+      hangman.play_hangman()
+      play_again = hangman.ask_play_again()
+      if not play_again:
+         break
+   print("Thanks for playing our game, see you later..")
+   sleep(1)
 
-   is_running = True
-   while is_running:
-      hangman.display_art(wrong_guess_count)
-      hangman.display_hint(hint)
-
-      while True:
-         guess = input("Enter letter: ")
-         flag = hangman.check_guess(guessed_letters, guess=guess)
-         if flag == "length_error":
-            print("letter should be 1 character long, try again..")
-            continue
-         elif flag == "alphabetic_error":
-            print("letter should be alphabetic, try again..")
-            continue
-         elif flag == "repeated_error":
-            print("you have already guessed that letter, try another one..")
-            continue
-         else:
-            break
-
-      if guess in random_word:
-         hint = hangman.update_hint(random_word, hint, guess)
-      else:
-         wrong_guess_count += 1
-      guessed_letters.append(guess)
-
-      flag = hangman.check_win_condition(wrong_guess_count, hint)
-      if flag == "won":
-         print(f"You won, total incorrect guesses: {wrong_guess_count}")
-         is_running = False
-      elif flag == "lost":
-         hangman.display_art(6) #full hangman
-         print(f"You lost, the word was: {random_word}")
-         is_running = False
 
 
 if __name__ == "__main__":
